3ceaa59daa3a44fd872efac10248709ee7ee9327
duilio.peroni <duilio.peroni@gmail.com>
2021-05-19 08:33:14 +0200

    Initial commit

---
+++ test.php
@@ -0,0 +1,13 @@
+<?php
+$usr_id=$_GET['usr_id'];
+echo $usr_id."<BR>";
+$conn= new MySQLi ('localhost','app','Xo82tj!e','blog');
+echo $conn ->errno."<BR>";
+$query= "SELECT * FROM post WHERE usr_id=$usr_id";
+echo $query."<BR>";
+$ris= $conn -> query($query);
+echo $ris->num_rows."<BR>";
+echo "post di $usr_id <BR>";
+while ($riga= $ris ->fetch_assoc()){
+	echo"post:".$riga['post_id']." contenuto:".$riga['post_content']." data:".$riga['post_dt']."<BR>";
+}
